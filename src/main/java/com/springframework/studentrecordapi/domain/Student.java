cc2b5886da31f95dc1d96ceb7c88771bbd4129d9
Fireryred <39081263+Fireryred@users.noreply.github.com>
2022-11-10 16:32:09 +0800

    Added Entity

---
+++ src/main/java/com/springframework/studentrecordapi/domain/Student.java
@@ -0,0 +1,29 @@
+package com.springframework.studentrecordapi.domain;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Student {
+    @Id
+    private String student_id;
+    @OneToMany(mappedBy = "student")
+    private Set<ClassDetails> class_details;
+}
