be061afbaa9ed3f6db98fbbb2820639c0a57d9b1
Fireryred <39081263+Fireryred@users.noreply.github.com>
2022-11-15 12:02:27 +0800

    Added Ignore unmapped properties

--- src/main/java/com/springframework/studentrecordapi/web/mappers/StudentMapper.java
+++ src/main/java/com/springframework/studentrecordapi/web/mappers/StudentMapper.java
@@ -1,11 +1,12 @@
 package com.springframework.studentrecordapi.web.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import com.springframework.studentrecordapi.domain.Student;
 import com.springframework.studentrecordapi.web.model.StudentBean;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface StudentMapper {
     StudentBean studentToStudentBean(Student st);
 
